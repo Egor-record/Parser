953616f48c15a5b125965b4929e6c1cc3afcdb7f
George Siaskov <egor.record@yandex.ru>
2020-03-09 14:37:00 +0300

    Add comments

--- src/main/java/App.kt
+++ src/main/java/App.kt
@@ -13,25 +13,41 @@ import kotlin.collections.ArrayList
 
 fun main() {
 
-    val chrome : String = "/Library/Chrome/chromedriver"
-    val siteURL : String = "https://1xstavka.ru/live/Football/"
-    val apiURL : String = "http://localhost:8080/api/v1/games/update"
-    val isHeadless : Boolean = true
 
+    val chrome : String = "/Library/Chrome/chromedriver" // Path to Chrome Browser
+    val siteURL : String = "https://1xstavka.ru/live/Football/"  // URL of the site we want to parse
+    val apiURL : String = "http://localhost:8080/api/v1/games/update" // URL of Server API
+    val isHeadless : Boolean = true // Use headless mode - no UI for browser
+
+    /**
+     * Method to open browser ? headless regime
+     */
     fun openBrowser(isHeadLess : Boolean) : WebDriver {
         System.setProperty("webdriver.chrome.driver", chrome)
         return ChromeDriver(ChromeOptions().setHeadless(isHeadLess))
     }
 
+    /**
+     * Open web page; Siteurl - url to webpage
+     */
     fun getUrl(browser : WebDriver) {
         return browser.get(siteURL)
     }
 
+    /**
+     * Array List of Games convert to JSON
+     */
     fun convertToJson(games: ArrayList<Game>) : String? {
         return Gson().toJson(games)
     }
 
+    /**
+     * Method to send json - string to Back-end API
+     */
     fun sendToAPI(games : String?) {
+        /*
+            Each HTTP Post Request in new thread
+         */
         Thread(Runnable {
 
             println(games.toString())
@@ -67,18 +83,26 @@ fun main() {
 
     }
 
+    /**
+     * Method which parse webpage
+     */
     fun parse() {
         val browser = openBrowser(isHeadless)
 
         getUrl(browser)
 
+        /**
+         * Each 10 seconds parse the page. The page changes its value using AJAX, so we don't need to GET it each time
+         */
         Timer().scheduleAtFixedRate(object : TimerTask() {
             override fun run() {
 
              val games : ArrayList<Game> = ArrayList()
 
              try {
-
+                /*
+                    Find elements by class name
+                 */
                  val elements = browser.findElements(
                      By.className("c-events-scoreboard")
                  )
@@ -99,8 +123,6 @@ fun main() {
                                      scoreSecondTeam = scoreDivs[1].text,
                                      league = "FIFA",
                                     betName = "1xBet"
-
-
                          );
 
                      games.add(game)
@@ -111,7 +133,9 @@ fun main() {
              } catch (e : StaleElementReferenceException) {
                  kotlin.io.println("No Element !")
              }
-
+                /**
+                 * Send JSON to API
+                 */
                 sendToAPI(convertToJson(games))
 
             }
